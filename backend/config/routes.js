a2c5640000a313101b3acecdccc252e7b12323db
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2022-04-10 18:47:59 -0300

    adicionando api de articles e category

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -7,6 +7,25 @@ module.exports = app => {
     app.route('/users/:id')
         .put(app.api.user.save)
 
-        
-    
+
+    app.route('/category')
+        .get(app.api.category.get)
+        .post(app.api.category.save)
+
+    app.route('/category/tree')
+        .get(app.api.category.getTree)
+
+    app.route('/category/:id')
+        .get(app.api.category.getById)
+        .put(app.api.category.save)
+        .delete(app.api.category.remove)
+
+    app.route('/articles')
+        .get(app.api.article.get)
+        .post(app.api.article.save)
+
+    app.route('/articles/:id')
+        .get(app.api.article.getById)
+        .put(app.api.article.save)
+        .delete(app.api.article.remove)
 }
